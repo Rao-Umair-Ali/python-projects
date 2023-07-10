7be30d0210b490bd872951610c6c67c8501b70e3
Rao Umair ALi <ruarua682@gmail.com>
2023-07-11 00:06:25 +0500

    .

---
+++ filling 11 7 23.py
@@ -0,0 +1,39 @@
+##-------------------------------------------------------------------
+##fp = "C:\\Users\\RUA\\Desktop\\python\\d.csv"
+##f = open(fp, "r")
+##md=f.readline()
+##c=1
+##d=1
+##md=f.readline()
+##list1=md.split(",")
+##pi=list1[6]
+##while True:
+##    md=f.readline()
+##    list1=md.split(",")
+##    if not md:
+##        break
+##    ni=list1[6]
+##    if pi==ni:
+##        c+=1
+##md=f.readline()
+##c=1
+##md=f.readline()
+##list1=md.split(",")
+##si=list1[6]
+##if si==pi and len(md)>0:
+##    md=f.readline()
+##    list1=md.split(",")
+##    si=list1[6]
+##else:
+##    while True:
+##        md=f.readline()
+##        list1=md.split(",")
+##        if not md:
+##            break
+##        ci=list1[6]
+##        if si==ci:
+##            d+=1
+##
+##print(pi,si)
+##print(c,d)
+
