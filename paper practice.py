01699c80f204628e8c60df5b8c67ce2af81bb76e
Rao Umair ALi <ruarua682@gmail.com>
2023-07-22 19:07:32 +0500

    .

---
+++ paper practice.py
@@ -0,0 +1,70 @@
+##================================1=========================
+##name=input("What is your name ")
+##print("Your name is :",name)
+
+##================================2=========================
+##Number1=int(input("give me a number : "))
+##Number2=int(input("give me another number : "))
+##print(Number1+Number2)
+
+##================================3=========================
+##Number_of_days=int(input("Give me number of days :"))
+##hours=Number_of_days*24
+##minutes=hours*60
+##seconds=minutes*60
+##print("Total hours:",hours,"\n"+"Total minutes:",minutes,"\n"+"Total seconds:",seconds)
+
+##================================4=========================
+##Number1=int(input("give me a number : "))
+##Number2=int(input("give me another number : "))
+##Number3=int(input("give me another number : "))
+##print((Number1+Number2)*Number3)
+
+##================================5=========================
+##Number1=int(input("give me a number : "))
+##if Number1>=10 and Number1<=20:
+##    print("Thank you :)")
+##else:
+##    print("Incorrect Answer :(")
+
+##================================6=========================
+##age=int(input("give me your age :"))
+##if age==18:
+##    print("You can vote")
+##elif age==17:
+##    print("You can learn to drive ")
+##elif age==16:
+##    print("You can buy a lottery")
+##elif age<16:
+##    print("You can go trick or treating ")
+##elif age>18:
+##    print("Give me age under 19")
+##else:
+##    print("give me age in numeric")
+
+##================================7=========================
+##num=int(input("give me your number:"))
+##if num==1:
+##    print("Thank you ")
+##elif num==2:
+##    print("Well done ")
+##elif num==3:
+##    print("Correct")
+##else:
+##    print("give me Number from 1 to 3 ")
+
+##================================8=========================
+##num=int(input("give me number for table :"))
+##i=1
+##while i<11:
+##    print(num,"x",i,"=",num*i)
+##    i+=1
+
+##================================8=========================
+num=int(input("give me number:"))
+i=50
+while i!=num:
+    ans=i-1
+    print(ans)
+    i-=1
+
