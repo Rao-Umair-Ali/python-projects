e18704ca31b8736122c0a84b944a01787c856a2d
Rao Umair ALi <ruarua682@gmail.com>
2023-07-25 07:22:52 +0500

    .

---
+++ list.py
@@ -0,0 +1,77 @@
+##-----------------------------Selection sorting-----------------------
+##a=[1,2,3,4,5,6,7]
+##i=0
+##while i<len(a):
+##    first=i
+##    c=a[i]*a[i]*a[i]
+##    a[i]=c
+##    i+=1
+##print(a)
+##    
+##-----------------------------Selection sorting-----------------------
+##a=[1,2,3,"",4,5,6,7]
+##i=0
+##while i<len(a):
+##    if a[i]=="":
+##        del a[i]
+##    i+=1
+##        
+##print(a)
+##   
+##-----------------------------Selection sorting-----------------------
+      
+##a=[5,6,8,9,1,25,4]
+##sv=int(input())
+##i=0
+##isFound=False
+##found=0
+##while i<len(a):
+##    if a[i]==sv:
+##        isFound=True
+##        found=i
+##    i+=1
+##if isFound==True:
+##    print("Found at  index :",found)
+##else:
+##    print("not found")
+##        
+
+
+##def hash(seatNum):
+##    dig=seatNum[-3:]
+##    num=int(dig)
+##    return num
+##list1=[-1]*200
+##N=input("seat num")
+##d=hash(N)
+##list1[d]=N
+##
+##    
+##print(list1)
+##
+##i=input("for searching")
+##myhash=hash(i)
+##if list1[myhash]==-1:
+##    print("not Found")
+##else:
+##    print("found",list1[myhash])
+
+
+##eq= lambda x:(x**3)+(x**2)-x+9
+##a=2
+##b=3
+##te=0.00000001
+##while(b-a)>te:
+##    c=(a+b)/2
+##    if eq(c)==0:
+##        break
+##    if eq(a)*eq(c)<0:
+##        b=c
+##    else:
+##        a=c
+##    sol=c
+print(sol)
+        
+
+
+    
